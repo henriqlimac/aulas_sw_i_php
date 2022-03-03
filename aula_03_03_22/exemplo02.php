fac8b64bbcf0cc0d03a936e7cb7229c6d535335c
Henrique Cardoso Lima <79847872+henriqlimac@users.noreply.github.com>
2022-03-03 08:53:10 -0300

    Add files via upload

---
+++ aula_03_03_22/exemplo02.php
@@ -0,0 +1,23 @@
+<?php
+
+$soma = ($valor = 4) + 5;
+
+$valor2 = 20;
+$valor3 = 33;
+
+$soma += 1;
+$soma += $valor2;
+$soma *= $valor3;
+$soma %= 100;
+
+$saudacao = "Bom ";
+$saudacao .= "dia!!! ";
+
+echo $saudacao . "Hoje vai ter " . $soma . "% tristeza!"
+
+
+
+
+
+
+?>
